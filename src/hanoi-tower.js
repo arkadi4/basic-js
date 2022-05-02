42e17974fe3ff3afa7e013b3e260625853b23ebd
arkadi4 <nik_arc@mail.ru>
2022-05-02 22:40:11 +0300

    feat:transform done

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,9 +14,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
+function calculateHanoi( disksNumber, turnsSpeed ) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let numTurns = Math.pow(2, disksNumber) - 1;
+  let numSeconds = Math.floor(numTurns / turnsSpeed * 3600);
+  return {turns: numTurns, seconds: numSeconds};
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (!(arr instanceof Array)) {
+    throw new Error(`'arr' parameter must be an instance of the Array!`);
+  }
+  let arr2 = [...arr];
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] == `--discard-next`) {
+      if (i == (arr2.length - 1)) {
+        continue;
+      } else {
+        arr2.splice(i + 1, 1);
+      }
+    }
+    if (arr2[i] == `--discard-prev`) {
+      if (i == 0) {
+        continue;
+      } else {
+        arr2.splice(i - 1, 1);
+      }
+    }
+    if (arr2[i] == `--double-next`) {
+      if (i == (arr2.length - 1)) {
+        continue;
+      } else {
+        arr2.splice(i + 2, 0, arr2[i + 1]);
+      }
+    }
+    if (arr2[i] == `--double-prev`) {
+      if (i == 0) {
+        continue;
+      } else {
+        arr2.splice(i - 2, 0, arr2[i - 1]);
+      }
+      i++;
+    }
+  }
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] == `--discard-next` || arr2[i] == `--discard-prev` || arr2[i] == `--double-next` || arr2[i] == `--double-prev`) {
+      arr2.splice(i, 1);
+      i--;
+    }
+  }
+
+  return arr2;
+
 }
 
 module.exports = {
